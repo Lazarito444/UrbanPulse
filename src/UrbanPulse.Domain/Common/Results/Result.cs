ead3822a860223a930524cb05b1cdf4b188a9425
Ariel Lázaro <ariellazaro444@gmail.com>
2026-02-11 19:28:32 -0400

    Add Result pattern implementation

---
+++ src/UrbanPulse.Domain/Common/Results/Result.cs
@@ -0,0 +1,52 @@
+﻿namespace UrbanPulse.Domain.Common.Results;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    protected Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None)
+            throw new InvalidOperationException();
+
+        if (!isSuccess && error == Error.None)
+            throw new InvalidOperationException();
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public TOut Match<TOut>(Func<TOut> onSuccess, Func<Error, TOut> onFailure) => IsSuccess
+        ? onSuccess()
+        : onFailure(Error);
+
+    public static Result Success() => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+}
+
+public class Result<TValue> : Result
+{
+    public TValue Value => IsSuccess
+        ? field!
+        : throw new InvalidOperationException("Can't access the value of a failed result");
+
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        Value = value;
+    }
+
+    public TOut Match<TOut>(Func<TValue, TOut> onSuccess, Func<Error, TOut> onFailure) => IsSuccess
+        ? onSuccess(Value)
+        : onFailure(Error);
+
+    public static Result<TValue> Success(TValue value) => new(value, true, Error.None);
+    public static new Result<TValue> Failure(Error error) => new(default, false, error);
+}
+
+public record Error(string Code, string Message)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+}
