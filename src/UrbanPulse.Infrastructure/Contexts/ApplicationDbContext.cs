23dcfefa10bd2729ccc97ff5b475e7ae19eb82f6
Ariel Lázaro <ariellazaro444@gmail.com>
2026-02-11 21:30:58 -0400

    Add EF Core to Infrastructure project

---
+++ src/UrbanPulse.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UrbanPulse.Infrastructure.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opts) : base(opts)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
