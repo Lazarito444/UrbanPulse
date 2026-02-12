19b8a38ab74145a064fcdcd057a32027d6467eaf
Ariel Lázaro <ariellazaro444@gmail.com>
2026-02-11 22:21:48 -0400

    Add dynamic expressions & specification support for query string params

---
+++ src/UrbanPulse.Domain/Common/DynamicExpressions/DynamicExpressionBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace UrbanPulse.Domain.Common.DynamicExpressions;
+
+public static class DynamicExpressionBuilder
+{
+    public static Expression<Func<T, bool>>? BuildFilter<T>(string propertyName, string operation, string value)
+    {
+        MethodInfo _startsWithMethod = typeof(string).GetMethod(nameof(string.StartsWith), [typeof(string)])!;
+        MethodInfo _endsWithMethod = typeof(string).GetMethod(nameof(string.EndsWith), [typeof(string)])!;
+        MethodInfo _containsMethod = typeof(string).GetMethod(nameof(string.Contains), [typeof(string)])!;
+
+        ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
+
+        PropertyInfo? propertyInfo = typeof(T).GetProperty(propertyName,
+            BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+        if (propertyInfo is null) return null;
+
+        MemberExpression propertyAccess = Expression.MakeMemberAccess(parameter, propertyInfo);
+
+        TypeConverter converter = TypeDescriptor.GetConverter(propertyInfo.PropertyType);
+        if (!converter.CanConvertFrom(typeof(string))) return null;
+
+        object? propertyValue = converter.ConvertFromInvariantString(value);
+        ConstantExpression constant = Expression.Constant(propertyValue);
+
+        // 3. Construir la expresión según el operador
+        Expression body = operation.ToLower() switch
+        {
+            "eq" => Expression.Equal(propertyAccess, constant),
+            "neq" => Expression.NotEqual(propertyAccess, constant),
+            "gt" => Expression.GreaterThan(propertyAccess, constant),
+            "gte" => Expression.GreaterThanOrEqual(propertyAccess, constant),
+            "lt" => Expression.LessThan(propertyAccess, constant),
+            "lte" => Expression.LessThanOrEqual(propertyAccess, constant),
+            "sw" => Expression.Call(propertyAccess, _startsWithMethod, constant),
+            "ew" => Expression.Call(propertyAccess, _endsWithMethod, constant),
+            "cont" => Expression.Call(propertyAccess, _containsMethod, constant),
+            _ => throw new InvalidOperationException($"Unsupported operation '{operation}' for dynamic filtering")
+        };
+
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
+
+    public static Expression<Func<T, object>>? BuildSort<T>(string propertyName)
+    {
+        ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
+        PropertyInfo? propertyInfo = typeof(T).GetProperty(propertyName,
+            BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+        if (propertyInfo is null) return null;
+
+        MemberExpression propertyAccess = Expression.MakeMemberAccess(parameter, propertyInfo);
+        UnaryExpression conversion = Expression.Convert(propertyAccess, typeof(object));
+
+        return Expression.Lambda<Func<T, object>>(conversion, parameter);
+    }
+}
---
+++ src/UrbanPulse.Domain/Common/Specifications/DynamicSpecification.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using UrbanPulse.Domain.Common.DynamicExpressions;
+using UrbanPulse.Domain.Common.Pagination;
+
+namespace UrbanPulse.Domain.Common.Specifications;
+
+public class DynamicSpecification<T> : BaseSpecification<T, T>
+{
+    public DynamicSpecification(QueryParameters parameters)
+    {
+        foreach (FilterParam filter in parameters.Filters)
+        {
+            Expression<Func<T, bool>>? expression = DynamicExpressionBuilder.BuildFilter<T>(filter.Field, filter.Operator, filter.Value);
+            if (expression is not null)
+            {
+                AddCriteria(expression);
+            }
+        }
+
+        foreach (SortParam sort in parameters.Sorts)
+        {
+            Expression<Func<T, object>>? expression = DynamicExpressionBuilder.BuildSort<T>(sort.Field);
+
+            if (expression is null) continue;
+
+            if (string.Equals(sort.Direction, "desc", StringComparison.OrdinalIgnoreCase))
+            {
+                AddOrderByDescending(expression);
+            }
+            else
+            {
+                AddOrderBy(expression);
+            }
+        }
+    }
+}
